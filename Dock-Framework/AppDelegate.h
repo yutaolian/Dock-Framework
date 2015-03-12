b8242fe759a5845b91645c255f3a47e1e1319032
lyt <675376983@qq.com>
2015-03-12 17:01:08 +0800

    Initial Commit

---
+++ Dock-Framework/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Dock-Framework
+//
+//  Created by lyt on 15/3/12.
+//  Copyright (c) 2015年 lyt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Dock-Framework/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Dock-Framework
+//
+//  Created by lyt on 15/3/12.
+//  Copyright (c) 2015年 lyt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
